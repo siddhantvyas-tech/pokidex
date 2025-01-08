2afac020c948b59e6f7c1e8ff8c551018d1ed7fb
siddhantvyas-tech <56452291+siddhantvyas-tech@users.noreply.github.com>
2025-01-08 23:54:37 +0530

    Uploaded App.js

---
+++ pokidex-UI/src/App.js
@@ -0,0 +1,14 @@
+// src/App.js
+import React from "react";
+import PokemonSearch from "./components/PokemonSearch";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <PokemonSearch />
+    </div>
+  );
+}
+
+export default App;
---
+++ pokidex-UI/src/index.js
@@ -0,0 +1,14 @@
+// src/index.js
+import React from "react";
+import ReactDOM from "react-dom";
+import { QueryClient, QueryClientProvider } from "react-query";
+import App from "./App";
+
+const queryClient = new QueryClient();
+
+ReactDOM.render(
+  <QueryClientProvider client={queryClient}>
+    <App />
+  </QueryClientProvider>,
+  document.getElementById("root")
+);
